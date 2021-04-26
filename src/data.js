9d1093373bfc3d25734eb6b1ed0e5fb4d0e4394b
장재영 <jaey5oung@naver.com>
2021-04-26 22:36:44 +0900

    4/26 shop all setting

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: 0,
+      title: 'Jordan 1 Court Purple',
+      content: 'Born in USA',
+      price: 300000,
+      picture:'https://image.frogbyhinter.com/products/20200221/20200221-4217719024.jpg'
+    },
+  {
+    id: 1,
+    title: 'Jordan 1 Shattred Back Board ',
+    content: 'Born in USA',
+    price: 278000,
+    
+    picture:'https://image.frogbyhinter.com/products/20200228/20200228-3713479994.jpg'
+  },
+  
+
+  {
+    id: 2,
+    title: 'Jordan 1 University Gold',
+    content: 'Born in USA',
+    price: 250000,
+    picture:'https://image.frogbyhinter.com/products/20200225/20200225-6106571042.jpg'
+  },
+];
